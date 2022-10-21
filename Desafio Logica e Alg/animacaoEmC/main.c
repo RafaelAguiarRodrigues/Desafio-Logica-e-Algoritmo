50d453b9ed4c52e6424e80841cd13e2b682b90b2
Rafael Aguiar Rodrigues <100633394+RafaelAguiarRodrigues@users.noreply.github.com>
2022-10-21 00:39:28 -0300

    colocando meu nome

--- Desafio Logica e Alg/animacaoEmC/main.c
+++ Desafio Logica e Alg/animacaoEmC/main.c
@@ -5,22 +5,22 @@
 #include "conio2.h"
 #include "modulo1.h"
 
-/*  */
+/* Rafael Aguiar Rodrigues - ADS */
 
 main() {
 	int i, y, x;
 	
-	imprimeTi ();
+	imprimeTi (); 
 	x = 7;
 	for (i = 0; i < 9; i++) {	
 		imprimeBemVindo (x);	
-		x += 8;
+		x += 8; 
 	}
 	
 	for (i = 14; i > 11; i--){	
-		textcolor(i);
+		textcolor(i); 
 		imprimeCoracao();
-		textcolor(i);
+		textcolor(i);  
 		imprimeCoracoes();	
 	}
 
